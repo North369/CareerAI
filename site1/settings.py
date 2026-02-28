88942233695619213d9f51233943b76af9ea2f80
North <bac54527@gmail.com>
2026-02-28 14:52:56 +0700

    Fix: Update settings and restore requirements

--- site1/settings.py
+++ site1/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8zu7^%aq!lw+l##o&fwo)9)p!x$)8=qiyzxoy6#e00nc03oo!2'
+SECRET_KEY = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*'] # Tạm thời để dấu sao như hôm qua bạn làm
+ALLOWED_HOSTS = ['*']# Tạm thời để dấu sao như hôm qua bạn làm
 
 
 # Application definition
@@ -120,11 +120,4 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATIC_ROOT = BASE_DIR / "staticfiles"
