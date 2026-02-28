d8fae655b9c4ab58889c6a80bd5f43503a2b7db6
North <bac54527@gmail.com>
2026-02-28 15:30:52 +0700

    Dut diem loi cu phap va cau hinh static

--- site1/settings.py
+++ site1/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Dòng này phải nằm ngay đây
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'site1.urls'
 
 TEMPLATES = [
@@ -118,3 +117,13 @@ USE_TZ = True
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+
+
+
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
