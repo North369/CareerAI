728dd27e68be465e4cae7e63cdb66189900c74fc
North <bac54527@gmail.com>
2026-02-28 15:48:00 +0700

    Switch to SQLite and fix secret key

--- site1/settings.py
+++ site1/settings.py
@@ -75,15 +75,10 @@ WSGI_APPLICATION = 'site1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'site1',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
